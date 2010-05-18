a77c4ca28e96e3523c8d440aa0b6f6eb6489bcdb
Thomas Moulard <thomas.moulard@gmail.com>
2010-05-18 15:25:04 +0200

    Add version checking mechanism.
    	One common problem when dealing with shared library is
    	keeping headers and libraries synchronized. Hard to debug
    	problems can be triggered by the inclusion of old headers.
    
    	The approach implemented in this patch keeps two strings
    	containing the package version: one is the header and
    	one compiled in the library. A macro is also provided
    	to allow the comparison of both values. This can be used
    	in libraries depending on hpp-util to make sure the library
    	is recent enough.
    
    	* configure.ac: Add dependency toward Boost.String.
    	* include/hpp/util/version.hh.in: New.
    	* src/Makefile.am: Compile new source file.
    	* src/version.cc: New.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

---
+++ src/version.cc
@@ -0,0 +1,55 @@
+// Copyright (C) 2010 by Thomas Moulard, CNRS.
+//
+// This file is part of the hpp-util.
+//
+// hpp-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// hpp-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with hpp-util.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "config.h"
+
+#include "hpp/util/version.hh"
+
+#include <string>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+
+namespace hpp
+{
+  namespace util
+  {
+    const char* version = PACKAGE_VERSION;
+
+    int checkVersion (const char* header_version)
+    {
+      std::vector<std::string> headerVersion;
+      std::vector<std::string> libraryVersion;
+
+      boost::split (headerVersion, header_version, boost::is_any_of("."));
+      boost::split (libraryVersion, version, boost::is_any_of("."));
+
+      unsigned s = std::max (headerVersion.size (), libraryVersion.size ());
+      headerVersion.resize (s);
+      libraryVersion.resize (s);
+
+      assert (headerVersion.size () == libraryVersion.size ());
+
+      for (unsigned i = 0; i < s; ++i)
+	{
+	  if (headerVersion != libraryVersion)
+	    return (headerVersion > libraryVersion) ? -1 : 1;
+	}
+      return 0;
+    }
+  } // end of namespace util.
+} // end of namespace hpp.
