71b960e06ab7d5cbde1c7a3ba89deaea55951870
Thomas Moulard <thomas.moulard@gmail.com>
2010-05-18 15:21:50 +0200

    Fix package name.
    	* include/hpp/util/timer.hh,
    	* src/debug.cc,
    	* src/timer.cc: Replace hpp/walkfootplanner by hpp-util.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

--- include/hpp/util/timer.hh
+++ include/hpp/util/timer.hh
@@ -1,13 +1,13 @@
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// hpp/walkfootplanner is distributed in the hope that it will be useful,
+// hpp-util is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with hpp/walkfootplanner.  If not, see <http://www.gnu.org/licenses/>.
+// along with hpp-util.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef HPP_UTIL_TIMER_HH
 # define HPP_UTIL_TIMER_HH
--- src/debug.cc
+++ src/debug.cc
@@ -1,19 +1,19 @@
 // Copyright (C) 2009, 2010 by Florent Lamiraux, Thomas Moulard, CNRS.
 //
-// This file is part of the hpp/walkfootplanner.
+// This file is part of the hpp-util.
 //
-// hpp/walkfootplanner is free software: you can redistribute it and/or modify
+// hpp-util is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// hpp/walkfootplanner is distributed in the hope that it will be useful,
+// hpp-util is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with hpp/walkfootplanner.  If not, see <http://www.gnu.org/licenses/>.
+// along with hpp-util.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config.h"
 
